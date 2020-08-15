b502a6c1ddbf0a6869052589764b343cfc8856f4
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2020-08-15 13:54:17 +0700

    add challenge switch statement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,60 @@
 public class Main {
     public static void main(String[] args) {
-        char switchValue = 'D';
+        defaultSwitchStatement();
+        challengeSwitchStatement(7);
+    }
 
-        switch (switchValue){
-            case 'A' :
+    public static void defaultSwitchStatement() {
+        char switchValue = 'D';
+        switch (switchValue) {
+            case 'A':
                 System.out.println("Value is A");
                 break;
-            case 'B'  :
+            case 'B':
                 System.out.println("Value is B");
                 break;
             case 'C':
                 System.out.println("Value is C");
                 break;
-            case 'D': case 'E' :
+            case 'D':
+            case 'E':
                 System.out.println("Value is D or E");
                 break;
             default:
                 System.out.println("Not Found");
                 break;
         }
+    }
+
+    public static void challengeSwitchStatement(int day) {
+        String nameDay;
+        switch (day) {
+            case 0:
+                nameDay = "Sunday";
+                break;
+            case 1:
+                nameDay = "Monday";
+                break;
+            case 2:
+                nameDay = "Tuesday";
+                break;
+            case 3:
+                nameDay = "Wednesday";
+                break;
+            case 4:
+                nameDay = "Thuesday";
+                break;
+            case 5:
+                nameDay = "Friday";
+                break;
+            case 6:
+                nameDay = "Saturday";
+                break;
+            default:
+                nameDay = "Invalid day";
+                break;
+        }
+        System.out.print(nameDay);
     }
 }
 
